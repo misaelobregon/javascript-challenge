f90a13c875dd4af3165a076c454ee0db3370bbd7
misaelobregon <43828674+misaelobregon@users.noreply.github.com>
2019-09-21 13:57:16 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,13 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-function buildTable(data){
-	//tbody.html("");	// To clear data
-	data.forEach((dataRow) => {
+function buildTable(dat){
+	tbody.html("");	// To clear data
+	dat.forEach((dataRow) => {
 	    // Append table row 'tr' to table body ‘tbody’
 	    var row = tbody.append("tr");
 	    // Use ‘object.entries’ to iterate values
-	    Object.entries(dataRow).forEach((value) => {
+	    Object.entries(dataRow).forEach(([key,value]) => {
 		    var cell = row.append("td");
 		    cell.text(value);
 	    });
@@ -34,36 +34,4 @@ function handleClick(){
 
 d3.selectAll("#filter-btn").on("click", handleClick);
 // Build Table with data.js 
-buildTable(tableData);
-
-
-
-
-
-
-
-  // BONUS: Calculate summary statistics for the age field of the filtered data
-
-  // First, create an array with just the age values
-  //var ages = filteredData.map(person => person.age);
-
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  //var mean = math.mean(ages);
-  //var median = math.median(ages);
-  //var mode = math.mode(ages);
-  //var variance = math.var(ages);
-  //var standardDeviation = math.std(ages);
-
-  // Then, select the unordered list element by class name
-  //var list = d3.select(".summary");
-
-  // remove any children from the list to
-  //list.html("");
-
-  // append stats to the list
-  //</script>/list.append("li").text(`Mean: ${mean}`);
-  //list.append("li").text(`Median: ${median}`);
-  //list.append("li").text(`Mode: ${mode}`);
-  //list.append("li").text(`Variance: ${variance}`);
-  //list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-//});
+//buildTable(tableData);
